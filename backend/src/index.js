2fe8df18645703a608562817c497ef6829416859
Igor Rômulo <igor.romulo@fretebras.com.br>
2020-01-19 23:01:13 -0300

    Feat: Turbinando integração backend e frontend
    	conjunto de alterações:
    	- Implementando socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 
 const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@omnistack-sobwj.mongodb.net/week100?retryWrites=true&w=majority",
@@ -19,4 +24,4 @@ mongoose.connect(
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+server.listen(3333);
